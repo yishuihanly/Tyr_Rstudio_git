ee8b3416de1e6d442ce920601c13b2c75dfdbf45
yishuihanly <yishuihanly@gmail.com>
2014-07-24 13:20:44 +0800

    second change of ui, without ","

--- ui.R
+++ ui.R
@@ -5,6 +5,8 @@ shinyUI(pageWithSidebar(
     sliderInput('mu', 'Guess at the mean',value = 70, 
                 min = 62, max = 74, step = 0.05,),
     sliderInput('std', 'Guess at the std',value = 70, 
+                min = 62, max = 74, step = 0.05,),
+    sliderInput('mu2', 'Guess at the mu2',value = 70, 
                 min = 62, max = 74, step = 0.05,)
   ),
   mainPanel(
