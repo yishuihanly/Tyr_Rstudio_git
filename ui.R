59a05f92dee16b2e7dcc9d48e7b57a94dd46c93b
yishuihanly <yishuihanly@gmail.com>
2014-07-24 13:20:44 +0800

    second change of ui

--- ui.R
+++ ui.R
@@ -5,7 +5,9 @@ shinyUI(pageWithSidebar(
     sliderInput('mu', 'Guess at the mean',value = 70, 
                 min = 62, max = 74, step = 0.05,),
     sliderInput('std', 'Guess at the std',value = 70, 
-                min = 62, max = 74, step = 0.05,)
+                min = 62, max = 74, step = 0.05,),
+    sliderInput('mu2', 'Guess at the mu2',value = 70, 
+                min = 62, max = 74, step = 0.05,),
   ),
   mainPanel(
     plotOutput('newHist')
